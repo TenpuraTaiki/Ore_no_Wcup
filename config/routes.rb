aa68cccc6045c7f650dd62c818556f1cea37c1ca
TenpuraTaiki <sleeping.3.fairies@gmail.com>
2018-07-07 15:01:35 +0900

    topとbeforeに手をつけ始めた

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,28 @@
 Rails.application.routes.draw do
+
+  root 'wcups#top'
+  get 'wcups/index'
+  get 'wcups/show'
+
   get 'favorites/new'
-
   get 'favorites/create'
-
   get 'favorites/edit'
-
   get 'favorites/update'
-
   get 'favorites/destroy'
 
+  get 'countries/before' => 'countries#before', as: "country_before"
   get 'countries/europe'
-
   get 'countries/africa'
-
   get 'countries/southamerica'
-
   get 'countries/concacaf'
-
   get 'countries/asia'
-
   get 'countries/oceania'
-
   get 'countries/32nd'
 
   get 'participations/index'
-
   get 'participations/shuffle'
 
-  get 'wcups/index'
 
-  get 'wcups/show'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
